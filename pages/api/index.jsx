469199e727c6271ede23bf25b300eb182cb79ced
Hieu Ngo <hieu.ngo12989@gmail.com>
2023-06-28 20:06:41 -0700

    generated environment template for health-tracker-app to convert

---
+++ pages/api/index.jsx
@@ -0,0 +1,22 @@
+import clientPromise from "../../lib/mongodb";
+
+const getServerSideProps = async () => {
+  try {
+    await clientPromise;
+    // `await clientPromise` will use the default database passed in the MONGODB_URI
+    // However you can use another database (e.g. myDatabase) by replacing the `await clientPromise` with the following code:
+    //
+    // `const client = await clientPromise`
+    // `const db = client.db("myDatabase")`
+    //
+    // Then you can execute queries against your database like so:
+    // db.find({}) or any of the MongoDB Node Driver commands
+
+    return { isConnected: true };
+  } catch (e) {
+    console.error(e);
+    return { isConnected: false };
+  }
+};
+
+export default getServerSideProps;
