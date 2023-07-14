d485f830f18054a5f3d27152da2227a55763290f
Hieu Ngo <hieu.ngo12989@gmail.com>
2023-07-14 13:31:41 -0700

    fixed icons in navBar and extracted used svgs to Icons.js (#9)

--- src/components/Icons.js
+++ src/components/Icons.js
@@ -1,12 +1,72 @@
 export const QuestionMark = ({ className }) => (
   <svg
-    className={`${className}`}
+    className={`self-center ${className}`}
     viewBox="0 0 1024 1024"
     fill="currentColor"
-    height="1.5em"
+    height="1em"
     width="1em"
   >
     <path d="M512 64C264.6 64 64 264.6 64 512s200.6 448 448 448 448-200.6 448-448S759.4 64 512 64zm0 820c-205.4 0-372-166.6-372-372s166.6-372 372-372 372 166.6 372 372-166.6 372-372 372z" />
     <path d="M623.6 316.7C593.6 290.4 554 276 512 276s-81.6 14.5-111.6 40.7C369.2 344 352 380.7 352 420v7.6c0 4.4 3.6 8 8 8h48c4.4 0 8-3.6 8-8V420c0-44.1 43.1-80 96-80s96 35.9 96 80c0 31.1-22 59.6-56.1 72.7-21.2 8.1-39.2 22.3-52.1 40.9-13.1 19-19.9 41.8-19.9 64.9V620c0 4.4 3.6 8 8 8h48c4.4 0 8-3.6 8-8v-22.7a48.3 48.3 0 0130.9-44.8c59-22.7 97.1-74.7 97.1-132.5.1-39.3-17.1-76-48.3-103.3zM472 732a40 40 0 1080 0 40 40 0 10-80 0z" />
+  </svg>
+);
+
+export const SignIn = ({ className }) => (
+  <svg
+    className={`self-center text-2xl ${className}`}
+    viewBox="0 0 24 24"
+    fill="currentColor"
+    height="1em"
+    width="1em"
+  >
+    <path d="M20 12a1 1 0 00-1-1h-7.59l2.3-2.29a1 1 0 10-1.42-1.42l-4 4a1 1 0 00-.21.33 1 1 0 000 .76 1 1 0 00.21.33l4 4a1 1 0 001.42 0 1 1 0 000-1.42L11.41 13H19a1 1 0 001-1zM17 2H7a3 3 0 00-3 3v14a3 3 0 003 3h10a3 3 0 003-3v-3a1 1 0 00-2 0v3a1 1 0 01-1 1H7a1 1 0 01-1-1V5a1 1 0 011-1h10a1 1 0 011 1v3a1 1 0 002 0V5a3 3 0 00-3-3z" />
+  </svg>
+);
+
+export const SignOut = ({ className }) => (
+  <svg
+    className={`self-center text-2xl ${className}`}
+    viewBox="0 0 24 24"
+    fill="currentColor"
+    height="1em"
+    width="1em"
+  >
+    <path d="M4 12a1 1 0 001 1h7.59l-2.3 2.29a1 1 0 000 1.42 1 1 0 001.42 0l4-4a1 1 0 00.21-.33 1 1 0 000-.76 1 1 0 00-.21-.33l-4-4a1 1 0 10-1.42 1.42l2.3 2.29H5a1 1 0 00-1 1zM17 2H7a3 3 0 00-3 3v3a1 1 0 002 0V5a1 1 0 011-1h10a1 1 0 011 1v14a1 1 0 01-1 1H7a1 1 0 01-1-1v-3a1 1 0 00-2 0v3a3 3 0 003 3h10a3 3 0 003-3V5a3 3 0 00-3-3z" />
+  </svg>
+);
+
+export const Home = ({ className }) => (
+  <svg
+    className={`self-center text-2xl ${className}`}
+    viewBox="0 0 24 24"
+    fill="currentColor"
+    height="1em"
+    width="1em"
+  >
+    <path fill="none" d="M0 0h24v24H0z" />
+    <path d="M20 20a1 1 0 01-1 1H5a1 1 0 01-1-1v-9H1l10.327-9.388a1 1 0 011.346 0L23 11h-3v9zm-2-1V9.157l-6-5.454-6 5.454V19h12zm-6-2l-3.359-3.359a2.25 2.25 0 113.182-3.182l.177.177.177-.177a2.25 2.25 0 113.182 3.182L12 17z" />
+  </svg>
+);
+
+export const Profile = ({ className }) => (
+  <svg
+    className={`self-center text-2xl ${className}`}
+    fill="none"
+    viewBox="0 0 24 24"
+    height="1em"
+    width="1em"
+  >
+    <path
+      fill="currentColor"
+      fillRule="evenodd"
+      d="M16 9a4 4 0 11-8 0 4 4 0 018 0zm-2 0a2 2 0 11-4 0 2 2 0 014 0z"
+      clipRule="evenodd"
+    />
+    <path
+      fill="currentColor"
+      fillRule="evenodd"
+      d="M12 1C5.925 1 1 5.925 1 12s4.925 11 11 11 11-4.925 11-11S18.075 1 12 1zM3 12c0 2.09.713 4.014 1.908 5.542A8.986 8.986 0 0112.065 14a8.984 8.984 0 017.092 3.458A9 9 0 103 12zm9 9a8.963 8.963 0 01-5.672-2.012A6.992 6.992 0 0112.065 16a6.991 6.991 0 015.689 2.92A8.964 8.964 0 0112 21z"
+      clipRule="evenodd"
+    />
   </svg>
 );
