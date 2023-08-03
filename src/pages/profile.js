56e1e9c6bbd1c4aad17b0a9a5a03a935dc019fe7
Hieu Ngo <hieu.ngo12989@gmail.com>
2023-08-03 14:57:23 -0700

    changed AccountCreation to AccountSettings, allowing editing profile (#12)

--- src/pages/profile.js
+++ src/pages/profile.js
@@ -8,6 +8,7 @@ import { Button } from "@nextui-org/react";
 import { useSession } from "next-auth/react";
 import { useFormatter } from "next-intl";
 import Link from "next/link";
+import { Setting } from "../components/Icons";
 import MealData from "./data/mealData";
 import SEW from "./data/sew";
 
@@ -16,6 +17,7 @@ const Profile = () => {
   const [age, setAge] = useState("");
   const [sewData, setSewData] = useState(false);
   const [mealsData, setMealsData] = useState(false);
+
   const router = useRouter();
   const { data: session, status } = useSession();
   const format = useFormatter();
@@ -43,27 +45,26 @@ const Profile = () => {
     date();
   }, [age]);
 
-  if (status === "loading") {
-    return <Layout>...Loading</Layout>;
-  }
-
+  if (status === "loading") return <Layout>...Loading</Layout>;
   if (status === "unauthenticated") router.push("/");
+  if (!userProfile) router.push("/");
 
-  return (
-    <>
-      <Head>
-        <title>Health Tracker - Profile</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+  if (userProfile.firstName)
+    return (
+      <>
+        <Head>
+          <title>HT - Profile</title>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
 
-      <Layout>
-        <div>
-          <div className="flex justify-center items start">
-            <div className="rounded border w-32 h-32 mr-6">
+        <Layout className="flex flex-col items-center">
+          {/* //! Profile Photo, Name, Email, Age, Height, Settings */}
+          <div className="flex justify-center">
+            <div className="w-40 h-auto mr-6">
               <img
                 src={userProfile.image}
                 alt="profile"
-                className="w-full h-full"
+                className="w-full h-full border border-white rounded-full"
               />
             </div>
 
@@ -72,75 +73,108 @@ const Profile = () => {
                 Name: {userProfile.firstName} {userProfile.lastName}
               </div>
               <div className="py-4">Email: {userProfile.email}</div>
-              <div>Age: {age}</div>
+              <div className="pb-4">Age: {age}</div>
+              <div>
+                Height: {userProfile.height.height_ft}ft.{" "}
+                {userProfile.height.height_in}in.
+              </div>
             </div>
+
+            <Link
+              href={{
+                pathname: "/AccountSettings",
+                query: {
+                  type: "update",
+                },
+              }}
+            >
+              <Setting className="p-0.5 ml-4" />
+            </Link>
           </div>
 
-          <div className="grid grid-cols-4 my-12">
-            <div className="col-span-4">
-              <span className="text-2xl">Your Dietary Goals:&nbsp;</span>
-              {userProfile.dietaryGoals}
-            </div>
-            <div className="col-span-2">
-              <span className="text-2xl">
-                Current Dietary Restrictions:&nbsp;
+          {/* //! Dietary & Health */}
+          <div className="flex p-2 my-12 w-full min-h-[8rem] max-w-4xl border rounded">
+            <div className="w-1/3 pl-2">
+              <span className="text-2xl font-semibold">
+                Your Dietary Goals:&nbsp;
               </span>
-              {userProfile.dietaryRestrictions
-                ? userProfile.dietaryRestrictions
-                : "none"}
+              {userProfile?.dietaryGoals?.map((goal) => {
+                return (
+                  <li key={goal} className="pl-2">
+                    {goal}
+                  </li>
+                );
+              })}
             </div>
-            <div className="col-span-2">
-              <span className="text-2xl">Health Complications:&nbsp;</span>
-              {userProfile.healthComplications}
+            <div className="w-2/3 pl-4">
+              <div className="">
+                <span className="text-2xl font-semibold pr-4">
+                  Current Dietary Restrictions:&nbsp;
+                </span>
+                {userProfile.dietaryRestrictions
+                  ? userProfile.dietaryRestrictions
+                  : "none"}
+              </div>
+              <div className="">
+                <span className="text-2xl font-semibold pr-4">
+                  Health Complications:&nbsp;
+                </span>
+                {userProfile.healthComplications}
+              </div>
             </div>
           </div>
-        </div>
 
-        <div className="flex px-4 justify-around">
-          {/* Input Data */}
-          <div className="flex flex-col items-center p-4">
-            Input data
-            <Link href="/data/addHealthData">
-              <Button className="m-4 hover:text-black hover:bg-green-500 ">
-                Input Wellness Data
+          <div className="flex px-4 justify-around">
+            {/*//! Input Data Section */}
+            <div className="flex flex-col items-center p-4">
+              <span className="text-xl font-bold uppercase underline">
+                Input data
+              </span>
+              <Link href="/data/addHealthData">
+                <Button className="m-4 hover:text-black hover:bg-green-500 ">
+                  Input Wellness Data
+                </Button>
+              </Link>
+              <Link href="/data/addMealData">
+                <Button className="m-4 hover:text-black hover:bg-green-500">
+                  Input Meals
+                </Button>
+              </Link>
+            </div>
+
+            {/*//! Show Data Section */}
+            <div className="flex flex-col items-center p-4">
+              <span className="text-xl font-bold uppercase underline">
+                View my Progress
+              </span>
+              <Button
+                className="m-4 hover:text-black hover:bg-green-500"
+                onPress={() => {
+                  setMealsData(false);
+                  setSewData(!sewData);
+                }}
+              >
+                Sleep/Exercise/Weight
               </Button>
-            </Link>
-            <Link href="/data/addMealData">
-              <Button className="m-4 hover:text-black hover:bg-green-500">
-                Input Meals
+              <Button
+                className="m-4 hover:text-black hover:bg-green-500"
+                onPress={() => {
+                  setSewData(false);
+                  setMealsData(!mealsData);
+                }}
+              >
+                Meals
               </Button>
-            </Link>
+            </div>
           </div>
 
-          {/* Show Data */}
-          <div className="flex flex-col items-center p-4">
-            View my Progress
-            <Button
-              className="m-4 hover:text-black hover:bg-green-500"
-              onPress={() => {
-                setMealsData(false);
-                setSewData(!sewData);
-              }}
-            >
-              Sleep/Exercise/Weight
-            </Button>
-            <Button
-              className="m-4 hover:text-black hover:bg-green-500"
-              onPress={() => {
-                setSewData(false);
-                setMealsData(!mealsData);
-              }}
-            >
-              Meals
-            </Button>
+          <div className="w-screen">
+            {sewData ? <SEW /> : null}
+            {mealsData ? <MealData /> : null}
           </div>
-        </div>
-
-        {sewData ? <SEW /> : null}
-        {mealsData ? <MealData /> : null}
-      </Layout>
-    </>
-  );
+        </Layout>
+      </>
+    );
 };
 
 export default Profile;
