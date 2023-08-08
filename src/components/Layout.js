18e4ec9e5df2c31ee2a974e99e0fa44e3e076d92
Hieu Ngo <hieu.ngo12989@gmail.com>
2023-08-07 17:01:40 -0700

    add negative margin to bring everything up

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -3,7 +3,7 @@ import React from "react";
 const Layout = ({ children, className = "" }) => {
   return (
     <div
-      className={`bg-blue-300/90 w-full min-h-screen inline-block p-12 ${className}`}
+      className={`bg-blue-300/90 w-full min-h-screen inline-block p-12 -mt-12 ${className}`}
     >
       {children}
     </div>
