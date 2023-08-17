2e70b82dc7d599d9453545bbe8f4626f910112e2
Hieu Ngo <hieu.ngo12989@gmail.com>
2023-08-17 15:23:22 -0700

    Added Google Analytics (#20)

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,14 +1,34 @@
-import "../styles/global.css";
-import Head from "next/head";
-import NavBar from "../components/NavBar";
+import { NextUIProvider } from "@nextui-org/react";
 import { SessionProvider } from "next-auth/react";
 import { NextIntlClientProvider } from "next-intl";
-import { NextUIProvider } from "@nextui-org/react";
+import Head from "next/head";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+import NavBar from "../components/NavBar";
+import * as ga from "../lib/ga";
+import "../styles/global.css";
 
 export default function App({
   Component,
   pageProps: { session, ...pageProps },
 }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      ga.pageview(url);
+    };
+    //When the component is mounted, subscribe to router changes
+    //and log those page views
+    router.events.on("routeChangeComplete", handleRouteChange);
+
+    // If the component is unmounted, unsubscribe
+    // from the event with the `off` method
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
     <>
       <Head>
