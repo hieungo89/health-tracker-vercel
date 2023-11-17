ddbca0f4a9e3dc88b323d5bb817d900a0759103d
Hieu Ngo <hieu.ngo12989@gmail.com>
2023-11-17 15:31:30 -0800

    change max width of layout to 1600px

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,5 +1,9 @@
 const Layout = ({ children, className = "" }) => {
-  return <div className={`m-auto px-4 py-12 ${className}`}>{children}</div>;
+  return (
+    <div className={`m-auto px-4 py-12 max-w-[1600px] ${className}`}>
+      {children}
+    </div>
+  );
 };
 
 export default Layout;
