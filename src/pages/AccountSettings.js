988cc3dd7dbb0d7c4ad13f6bde05f3028de09449
Hieu Ngo <hieu.ngo12989@gmail.com>
2023-11-16 16:13:56 -0800

    Reconfigure and fix new user profile creation flow

--- src/pages/AccountSettings.js
+++ src/pages/AccountSettings.js
@@ -1,4 +1,5 @@
 import Layout from "@components/Layout";
+import NewProfile from "@components/NewProfile";
 import axios from "axios";
 import { useSession } from "next-auth/react";
 import Head from "next/head";
@@ -42,6 +43,48 @@ const AccountCreation = ({ type }) => {
       .catch((error) => console.log(error));
   };
 
+  const NameField = ({ label, text }) => (
+    <label className="col-span-1" htmlFor={label}>
+      *{text}:
+      <input
+        className="mx-1 px-2 bg-blue-100"
+        type="text"
+        name={label}
+        placeholder={text}
+        required
+      />
+    </label>
+  );
+
+  const GoalField = ({ name, placeholder, required }) => (
+    <li className="mx-4">
+      <input
+        className="p-2 bg-blue-100 md:text-sm"
+        type="text"
+        rows="4"
+        cols="40"
+        name={name}
+        placeholder={placeholder}
+        required={required}
+      />
+    </li>
+  );
+
+  const TextAreaField = ({ label, text }) => (
+    <>
+      <label htmlFor={label}>
+        {text} (<em>optional</em>):
+      </label>
+      <textarea
+        className="p-2 bg-blue-100"
+        type="textbox"
+        rows="4"
+        cols="40"
+        name={label}
+      />
+    </>
+  );
+
   const getUserData = async () => {
     const { data } = await axios.get(`/api/user?email=${session?.user.email}`);
     setUserProfile(data);
@@ -72,177 +115,15 @@ const AccountCreation = ({ type }) => {
   if (userProfile === "") router.push("/profile");
 
   //! New Account Profile Setup
-  if (session && option === "create") {
+  if (session && option === "create")
     return (
-      <>
-        <Head>
-          <title>HT - create account</title>
-          <meta
-            name="create account"
-            content="New users need to create an account with all the info provided."
-          />
-          <link rel="icon" href="/favicon.ico" />
-        </Head>
-
-        <Layout className="flex flex-col items-center">
-          <div className="w-fit">
-            <h1
-              className=" text-5xl pb-10 text-center underline
-              md:text-4xl sm:text-3xl sm:pb-4 xs:text-2xl"
-            >
-              New User Profile Creation
-            </h1>
-
-            <h5>Please fill these fields to get started.</h5>
-            <p>Fields with * are required</p>
-
-            <form className="" onSubmit={(e) => handleSubmit(e)}>
-              {/* Name */}
-              <div
-                className="grid grid-cols-2 justify-around py-4
-                md:text-sm sm:grid-cols-1"
-              >
-                <label className="col-span-1" htmlFor="firstName">
-                  *First Name:&nbsp;
-                  <input
-                    className="px-2 bg-blue-100"
-                    type="text"
-                    name="firstName"
-                    placeholder="first name"
-                    required
-                  />
-                </label>
-                <label className="col-span-1 sm:my-2" htmlFor="lastName">
-                  *Last Name:&nbsp;
-                  <input
-                    className="px-2 bg-blue-100"
-                    type="text"
-                    name="lastName"
-                    placeholder="last name"
-                    required
-                  />
-                </label>
-              </div>
-              <div
-                className="grid grid-cols-2 pb-4
-                md:text-sm sm:grid-cols-1"
-              >
-                {/* Birthday */}
-                <label className="col-span-1" htmlFor="birthday">
-                  *Birthday:&nbsp;
-                  <input
-                    className="px-2 text-center bg-blue-100"
-                    type="date"
-                    name="birthday"
-                    max={new Date().toISOString().slice(0, 10)}
-                    required
-                  />
-                </label>
-                {/* Height */}
-                <div className="col-span-1 sm:my-2">
-                  <span>*Height:&nbsp;</span>
-                  <input
-                    className="text-center mr-1 bg-blue-100"
-                    type="number"
-                    min="1"
-                    max="10"
-                    name="height_ft"
-                    placeholder="5"
-                    required
-                  />
-                  <label htmlFor="height_ft">ft. </label>
-                  <input
-                    className="text-center mr-1 bg-blue-100"
-                    type="number"
-                    min="0"
-                    max="11"
-                    name="height_in"
-                    placeholder="0"
-                    required
-                  />
-                  <label htmlFor="height_in">in.</label>
-                </div>
-              </div>
-              <div className="flex flex-col md:text-sm">
-                {/* Dietary Goals */}
-                <label htmlFor="dietaryGoals">*Dietary Goals:</label>
-                <ol className="flex sm:flex-col">
-                  <li className="mr-8">
-                    <input
-                      className="p-2 bg-blue-100 md:text-sm"
-                      type="text"
-                      rows="4"
-                      cols="40"
-                      name="dietaryGoals1"
-                      placeholder="Goal 1"
-                      required
-                    />
-                  </li>
-                  <li className="mr-8">
-                    <input
-                      className="p-2 bg-blue-100 md:text-sm"
-                      type="text"
-                      rows="4"
-                      cols="40"
-                      name="dietaryGoals2"
-                      placeholder="(Optional)"
-                    />
-                  </li>
-                  <li>
-                    <input
-                      className="p-2 bg-blue-100 md:text-sm"
-                      type="text"
-                      rows="4"
-                      cols="40"
-                      name="dietaryGoals3"
-                      placeholder="(Optional)"
-                    />
-                  </li>
-                </ol>
-                {/* Dietary Restrictions */}
-                <label htmlFor="dietaryRestrictions">
-                  Dietary Restrictions (<em>optional</em>):
-                </label>
-                <textarea
-                  className="p-2 bg-blue-100"
-                  type="textbox"
-                  rows="4"
-                  cols="40"
-                  name="dietaryRestrictions"
-                />
-                {/* Health Complications */}
-                <label htmlFor="healthComplications">
-                  Health Complications (<em>optional</em>):
-                </label>
-                <textarea
-                  className="p-2 bg-blue-100"
-                  type="textbox"
-                  rows="4"
-                  cols="40"
-                  name="healthComplications"
-                />
-              </div>
-              <div className="flex">
-                <input
-                  className="bg-blue-200 font-semibold border border-solid border-white p-2 my-4 rounded
-                hover:bg-green-500 hover:border-black sm:text-sm"
-                  type="submit"
-                  value="Submit Profile"
-                />
-                <Link
-                  href="/"
-                  className="bg-blue-200 font-semibold ml-4 border border-solid border-white p-2 my-4 rounded
-                hover:bg-red-500 hover:border-black sm:text-sm"
-                >
-                  Go Back
-                </Link>
-              </div>
-            </form>
-          </div>
-        </Layout>
-      </>
+      <NewProfile
+        handleSubmit={(e) => handleSubmit(e)}
+        NameField={NameField}
+        GoalField={GoalField}
+        TextAreaField={TextAreaField}
+      />
     );
-  }
 
   //! Update User Profile
   if (session && option === "update" && userProfile.googleName) {
