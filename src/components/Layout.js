fcb9d2d31c83638ec8ff7e1af2c54469e307c11c
Hieu Ngo <hieu.ngo12989@gmail.com>
2023-11-17 18:23:47 -0800

    set min height for all pages

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,6 +1,8 @@
 const Layout = ({ children, className = "" }) => {
   return (
-    <div className={`m-auto px-4 py-12 max-w-[1600px] ${className}`}>
+    <div
+      className={`m-auto px-4 py-12 max-w-[1600px] min-h-[90vh] ${className}`}
+    >
       {children}
     </div>
   );
