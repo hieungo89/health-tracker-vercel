2091e970c561d5f48f57ad8f07b479b3205aa32a
Hieu Ngo <hieu.ngo12989@gmail.com>
2023-11-20 17:56:21 -0800

    Fix styling

--- src/pages/profile.js
+++ src/pages/profile.js
@@ -11,6 +11,7 @@ import Head from "next/head";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import { useEffect, useState, useCallback } from "react";
+import Image from "next/image";
 
 const Profile = () => {
   const [userProfile, setUserProfile] = useState({});
@@ -73,10 +74,12 @@ const Profile = () => {
         <div className="flex w-full max-w-3xl">
           <div className="flex w-full justify-evenly sm:flex-col sm:items-center">
             <div className="w-40 h-auto">
-              <img
+              <Image
                 src={userProfile.image}
                 alt="profile-pic"
                 className="w-full h-auto rounded-full"
+                width="100"
+                height="100"
               />
             </div>
 
@@ -91,7 +94,7 @@ const Profile = () => {
                 <b>Age:</b> {age}
               </div>
               <div>
-                <b>Height:</b>
+                <b>Height:</b>&nbsp;
                 {userProfile.height.height_ft}ft. {userProfile.height.height_in}
                 in.
               </div>
