c77e47e59c4f95eb84eb467434f73798b03ce7c2
Hieu Ngo <hieu.ngo12989@gmail.com>
2024-03-01 10:47:34 -0800

    Fix updates (#44)
    * Modify components to specific usage. Correct adding food card flow.
    
    * Separate addIngredient and editIngredient flow
    
    * Fix Alert message

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -1,5 +1,6 @@
 import { QuestionMark } from "@components/Icons";
 import { Card, Popover, Text } from "@nextui-org/react";
+import { useState, useEffect } from "react";
 
 const Popup = ({ text, placement, card }) => {
   return (
@@ -20,4 +21,33 @@ const Popup = ({ text, placement, card }) => {
   );
 };
 
-export default Popup;
+const PopupErrorMessage = ({ text, placement, card }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  useEffect(() => {
+    setIsOpen(true);
+  }, []);
+
+  return (
+    <Popover
+      placement={placement}
+      isOpen={isOpen}
+      onOpenChange={(open) => setIsOpen(open)}
+    >
+      <Popover.Trigger>
+        <Text>
+          <QuestionMark className="mx-1 text-red-600" />
+        </Text>
+      </Popover.Trigger>
+      <Popover.Content>
+        {card ? (
+          <Card css={{ mw: "400px", padding: "8px" }}>{text}</Card>
+        ) : (
+          <>{text}</>
+        )}
+      </Popover.Content>
+    </Popover>
+  );
+};
+
+export { Popup, PopupErrorMessage };
