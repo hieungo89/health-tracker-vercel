4c7718e50c286984f42075d88e91c2719fb98a18
Hieu Ngo <hieu.ngo12989@gmail.com>
2024-08-19 14:47:30 -0700

    update splash page to be more appealing

--- src/components/SplashPage.js
+++ src/components/SplashPage.js
@@ -5,6 +5,12 @@ import { signIn, useSession } from "next-auth/react";
 import Image from "next/image";
 import { useRouter } from "next/router";
 import Logo from "../components/Logo.js";
+import moon from "@images/moon.jpg";
+import fitness from "@images/fitness.jpg";
+import scale from "@images/scale.jpg";
+import meals from "@images/meals.png";
+import goals from "@images/goals.png";
+import update from "@images/update.png";
 
 const cssStyles = {
   title: `font-semibold text-primary-light font-georgia text-9xl lg:text-8xl md:text-7xl sm:text-6xl xs:text-4xl`,
@@ -20,11 +26,13 @@ const SplashPage = () => {
   const router = useRouter();
   const { data: session } = useSession();
 
+  const quote = "Your personal health tracker at your Fingertips!";
+
   return (
     <Layout className="flex flex-col justify-center items-center bg-white">
-      <div className="flex flex-col items-center pt-4 md:pt-2">
+      <div className="flex flex-col items-center pt-4 md:pt-2 appear">
         <Logo
-          className={`${cssStyles.title} cursor-default`}
+          className={`${cssStyles.title} cursor-default logo`}
           imgClass="w-[1em]"
         />
         <p className={cssStyles.appDescription}>
@@ -40,22 +48,30 @@ const SplashPage = () => {
         />
       </div>
 
-      <div className="flex flex-col w-full bg-secondary-dark py-60 md:py-32">
-        <div className={cssStyles.headerLight}>
-          Your personal health tracker at your Fingertips!
+      <div className="flex flex-col w-full bg-secondary-dark py-40 md:py-24">
+        <div className={`${cssStyles.headerLight}`}>
+          {quote.split("").map((l, i) => (
+            <span
+              key={l + i}
+              className="animated-textColor"
+              style={{ "--position": i + 1 }}
+            >
+              {l}
+            </span>
+          ))}
         </div>
         <div className="flex space-x-16 px-8 pt-[1em] md:flex-col md:space-x-0">
-          <p className={cssStyles.contentText}>
+          <p className={`${cssStyles.contentText} animated-text`}>
             Tired of losing track of your health goals? We&#39;ve got your back!
             Health.me helps you record and monitor your sleep, exercise, weight,
             and meals like a pro.
           </p>
-          <p className={cssStyles.contentText}>
+          <p className={`${cssStyles.contentText} animated-text`}>
             With our intuitive interface, tracking your habits couldn&#39;t be
             easier! Stay on top of your goals, identify patterns, and smash
             those personal milestones.
           </p>
-          <p className={cssStyles.contentText}>
+          <p className={`${cssStyles.contentText} animated-text`}>
             Wave goodbye to spreadsheets and notebooks - it&#39;s time to
             revolutionize the way you manage your health journey for better
             well-being.
@@ -63,38 +79,46 @@ const SplashPage = () => {
         </div>
       </div>
 
-      <div className="flex flex-col w-full bg-gray-50 py-40 md:py-20">
-        <div className={cssStyles.headerDark}>Track Your Health</div>
+      <div className="flex flex-col w-full bg-gray-50 py-40 md:py-24">
+        <div className={`${cssStyles.headerDark} animated-text`}>
+          Track Your Health
+        </div>
         <div className="grid grid-cols-2 m-12 sm:grid-cols-1">
           <TrackHealthContent
             title="Monitor Sleep"
             description="Catch those Z’s by keeping track of your sleep habits.
                 Input hours and minutes to analyze your sleep cycles."
+            image={moon}
           />
           <TrackHealthContent
             title="Track Exercise"
             description="Keep hustlin’ and hit your fitness goals.
-                Input exercise hours and minutes to stay on top."
+            Input exercise hours and minutes to stay on top."
+            image={fitness}
           />
           <TrackHealthContent
             title="Watch Your Weight"
             description="Stay informed about your weight fluctuations.
-                Input weight and time taken for real-time tracking."
+            Input weight and time taken for real-time tracking."
+            image={scale}
           />
           <TrackHealthContent
             title="Manage Meals"
             description="Fine-tune your meal intakes.
-                Search through a database for nutritional values of ingredients."
+            Search through a database for nutritional values of ingredients."
+            image={meals}
           />
           <TrackHealthContent
             title="Goal Setting"
             description="Set customized goals for yourself
-                and track your progress effortlessly."
+            and track your progress effortlessly."
+            image={goals}
           />
           <TrackHealthContent
             title="Easy Updates"
             description="Got a change of heart?
-                Updating your information is a piece of cake."
+            Updating your information is a piece of cake."
+            image={update}
           />
         </div>
       </div>
@@ -125,7 +149,7 @@ const SplashPage = () => {
       </div>
 
       <div className="flex flex-col w-full bg-primary-light py-20 md:py-12">
-        <div className={cssStyles.headerLight}>
+        <div className={`${cssStyles.headerLight} animate-refuel`}>
           FUEL YOUR MOMENTUM WITH FRESH HEALTH TIPS
         </div>
         <form className="flex flex-col items-center space-y-4 pt-8">
@@ -144,13 +168,20 @@ const SplashPage = () => {
 
 export default SplashPage;
 
-const TrackHealthContent = ({ title, description }) => {
+const TrackHealthContent = ({ title, description, image }) => {
   return (
-    <div className="col-span-1 w-[75%] pb-[2em] sm:w-[80%]">
-      <div className="font-semibold text-2xl font-helvetica md:text-lg">
-        {title}
+    <div className="col-span-1 flex justify-between w-[90%] pb-[2em] sm:w-[100%]">
+      <div>
+        <div className="font-semibold text-2xl font-helvetica md:text-lg">
+          {title}
+        </div>
+        <p className=" text-lg font-helvetica md:text-base">{description}</p>
       </div>
-      <p className="text-lg font-helvetica md:text-base">{description}</p>
+      <Image
+        src={image}
+        alt={image}
+        className="reveal w-28 h-28 lg:w-20 lg:h-20 sm:w-24 sm:h-24"
+      />
     </div>
   );
 };
